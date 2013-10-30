47e1eef5e9fbbbe37beb0b107107d6af9b13b32c
Myeongseok Baek <msbaek@daumcorp.com>
2013-10-30 14:54:18 +0900

    2. Basic Form
    bidirectional data binding

---
+++ test/e2e/basic-form-scenario.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+
+describe('simple form', function () {
+
+    beforeEach(function () {
+        browser().navigateTo('../../app/basic-form.html');
+    });
+
+
+    it('should shoud debug', function () {
+        expect(binding('user')).toMatch(/John Smith/);
+    });
+
+    it('should add contact', function () {
+        using('.example').element('a:contains(add)').click();
+        using('.example div:last').input('contact.value').enter('you@example.org');
+        expect(binding('user')).toMatch(/\(234\) 555\-1212/);
+        expect(binding('user')).toMatch(/you@example.org/);
+    })
+
+    it('should validate zip', function () {
+        expect(using('.example').
+            element(':input[ng\\:model="user.address.zip"]').
+            prop('className')).not().toMatch(/ng-invalid/);
+        using('.example').input('user.address.zip').enter('abc');
+        expect(using('.example').
+            element(':input[ng\\:model="user.address.zip"]').
+            prop('className')).toMatch(/ng-invalid/);
+    })
+
+    it('should validate state', function () {
+        expect(using('.example').element(':input[ng\\:model="user.address.state"]').
+            prop('className'))
+            .not().toMatch(/ng-invalid/);
+        using('.example').input('user.address.state').enter('XXX');
+        expect(using('.example').element(':input[ng\\:model="user.address.state"]')
+            .prop('className')).toMatch(/ng-invalid/);
+    });
+});
--- test/e2e/hello-world-scenario.js
+++ test/e2e/hello-world-scenario.js
@@ -2,14 +2,14 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('angularjs cookbook', function () {
+describe('hello world', function () {
 
     beforeEach(function () {
         browser().navigateTo('../../app/hello-world.html');
     });
 
 
-    it('hello-world: should change the binding when user enters text', function () {
+    it('should change the binding when user enters text', function () {
         expect(binding('name')).toEqual('World');
         input('name').enter('angular');
         expect(binding('name')).toEqual('angular');
