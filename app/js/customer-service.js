c9d76769bc9207ed9110960fd1060fb01f06bc23
Myeongseok Baek <msbaek@daumcorp.com>
2013-11-05 15:32:16 +0900

    custom filter

--- app/js/customer-service.js
+++ app/js/customer-service.js
@@ -19,3 +19,19 @@ customServiceApp.controller('CustomServiceController', ['$scope', 'Math', 'Str',
     $scope.result = Math.multiply(a, b);
     $scope.greeting = Str.greeting();
 }]);
+
+customServiceApp.filter('reverse', function() {
+   return function(input, uppercase, postfix) {
+       var out = '';
+       for(var i = 0; i < input.length; i++) {
+           out = input.charAt(i) + out;
+       }
+       if(uppercase) {
+           out = out.toUpperCase();
+       }
+       if(postfix) {
+           return out + postfix;
+       }
+       return out;
+   }
+});
